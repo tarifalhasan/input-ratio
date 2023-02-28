880a9c60fe790a9fb1f4746aaa2a3ffeb5e05aa6
tarifalhasan <tarifalhasan999@gmail.com>
2023-02-28 22:54:21 +0600

    frist commit

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,9 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
+import Head from 'next/head';
+import Image from 'next/image';
+import { Inter } from 'next/font/google';
+import styles from '@/styles/Home.module.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
@@ -14,110 +14,48 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className={styles.main}>
-        <div className={styles.description}>
-          <p>
-            Get started by editing&nbsp;
-            <code className={styles.code}>src/pages/index.tsx</code>
-          </p>
-          <div>
-            <a
-              href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              By{' '}
-              <Image
-                src="/vercel.svg"
-                alt="Vercel Logo"
-                className={styles.vercelLogo}
-                width={100}
-                height={24}
-                priority
-              />
-            </a>
-          </div>
+      <div className="container mx-auto px-10">
+        <div className="grid-8">
+          <ul>
+            <li>
+              <label className="inline-flex items-center cursor-pointer">
+                <input type="radio" className="hidden" name="radio" />
+                <div className="relative w-[24.5px] h-[24.5px] mr-2">
+                  <div className="absolute inset-0 rounded-full bg-transparent border-2 border-gray-400 transition-colors duration-300 hover:border-black check-hover"></div>
+                  <div className="absolute inset-[3px] rounded-full bg-blue-500 opacity-0 transition-opacity checked"></div>
+                </div>
+                <span className="text-gray-700 font-medium">
+                  Circle Input Ratio Button
+                </span>
+              </label>
+            </li>
+            <li>
+              <label className="inline-flex items-center cursor-pointer">
+                <input type="radio" className="hidden" name="radio" />
+                <div className="relative w-[24.5px] h-[24.5px]   mr-2">
+                  <div className="absolute inset-0 rounded-full bg-transparent border-2 border-gray-400 transition-colors duration-300 hover:border-black check-hover"></div>
+                  <div className="absolute inset-[3px] hover:border-black rounded-full bg-blue-500 opacity-0 transition-opacity checked"></div>
+                </div>
+                <span className="text-gray-700 font-medium">
+                  Circle Input Ratio Button
+                </span>
+              </label>
+            </li>
+            <li>
+              <label className="inline-flex items-center cursor-pointer">
+                <input type="radio" className="hidden" name="radio" />
+                <div className="relative w-[24.5px] h-[24.5px] mr-2">
+                  <div className="absolute inset-0 rounded-full bg-transparent border-2 border-gray-400 transition-colors duration-300 hover:border-black check-hover"></div>
+                  <div className="absolute inset-[3px] rounded-full bg-blue-500 opacity-0 transition-opacity checked"></div>
+                </div>
+                <span className="text-gray-700 font-medium">
+                  Circle Input Ratio Button
+                </span>
+              </label>
+            </li>
+          </ul>
         </div>
-
-        <div className={styles.center}>
-          <Image
-            className={styles.logo}
-            src="/next.svg"
-            alt="Next.js Logo"
-            width={180}
-            height={37}
-            priority
-          />
-          <div className={styles.thirteen}>
-            <Image
-              src="/thirteen.svg"
-              alt="13"
-              width={40}
-              height={31}
-              priority
-            />
-          </div>
-        </div>
-
-        <div className={styles.grid}>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Docs <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Find in-depth information about Next.js features and&nbsp;API.
-            </p>
-          </a>
-
-          <a
-            href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Learn <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Learn about Next.js in an interactive course with&nbsp;quizzes!
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Templates <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Discover and deploy boilerplate example Next.js&nbsp;projects.
-            </p>
-          </a>
-
-          <a
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <h2 className={inter.className}>
-              Deploy <span>-&gt;</span>
-            </h2>
-            <p className={inter.className}>
-              Instantly deploy your Next.js site to a shareable URL
-              with&nbsp;Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
+      </div>
     </>
-  )
+  );
 }
